ce99abcb5f5027045eb089b7bce149f8cb5c7737
callme-rokib <scomp.id@gmail.com>
2022-06-08 19:53:23 +0700

    add function greeting

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/callme-rokib/go-greetings
+
+go 1.18
---
+++ greetings.go
@@ -0,0 +1,9 @@
+package go_greetings
+
+func sayGoogMoring() string {
+	return "good morning"
+}
+
+func sayHello() string {
+	return "Hello"
+}
